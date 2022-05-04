55251866f8998702a3a5a1fc27f40b0736ca04f0
Cyber <neven.lukic8@gmail.com>
2022-05-04 17:47:59 +0200

    login and register

--- frontend/src/store/globalStore.js
+++ frontend/src/store/globalStore.js
@@ -3,6 +3,10 @@ import create from 'zustand';
 const useStore = create(set => ({
   currentSport: '',
   setCurrentSport: currentSport => set({ currentSport }),
+
+  user: {},
+  setUser: user => set({ user }),
+  clearUser: user => set({ user: {} }),
 }));
 
 export { useStore };
